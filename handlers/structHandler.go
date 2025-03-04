bea839fcddf9f68baf45810c63a8633c5948c2b5
exit <cchum900@gmail.com>
2025-03-04 11:03:04 +0100

    Remade the structs for the future?, hopefully, might need to change depending on the needs.

--- handlers/structHandler.go
+++ handlers/structHandler.go
@@ -1,22 +1,54 @@
 package handlers
 
-type yearlyPop struct {
-	Year  string `json:"year,omitempty"`
-	Value int    `json:"value,omitempty"`
+type CountryName struct {
+	CountryName string `json:"common"`
 }
 
-type Info struct {
-	Name       string `json:"name"`
-	Continent  string `json:"continents"`
-	Population string `json:"population"`
-	Languages  string `json:"languages,"`
-	Bordering  string `json:"bordering,omitempty"`
-	Flag       string `json:"flag"`
-	Capital    string `json:"capital"`
-	Cities     string `json:"cities"`
+type CountryFlag struct {
+	Png string `json:"png"`
 }
 
-type Population struct {
-	Mean   int       `json:"mean"`
-	Values yearlyPop `json:"values"`
+type Cities struct {
+	Cities []string `json:"data"`
+}
+
+type RestCountries struct {
+	Name       CountryName       `json:"name"`
+	Continents []string          `json:"continents"`
+	Population int               `json:"population"`
+	Languages  map[string]string `json:"languages"`
+	Borders    []string          `json:"borders"`
+	Capital    []string          `json:"capital"`
+	Flag       CountryFlag       `json:"flags"`
+}
+
+type InfoResponse struct {
+	Name       CountryName       `json:"name"`
+	Continents []string          `json:"continents"`
+	Population int               `json:"population"`
+	Languages  map[string]string `json:"languages"`
+	Bordering  []string          `json:"bordering,omitempty"`
+	Flag       string            `json:"flag"`
+	Capital    string            `json:"capital"`
+	Cities     []string          `json:"cities"`
+}
+
+type PopulationData struct {
+	PopulationInfo []map[string]int `json:"populationCounts"`
+}
+
+type GetPopulationData struct {
+	Data PopulationData `json:"data"`
+}
+
+type PopulationInfoResponse struct {
+	Mean   int              `json:"mean"`
+	Values []map[string]int `json:"values"`
+}
+
+type StatusResponse struct {
+	CountriesNowApi  int    `json:"countriesNowApi"`
+	RestCountriesApi int    `json:"restCountriesApi"`
+	Version          string `json:"version"`
+	Uptime           string `json:"uptime"`
 }
